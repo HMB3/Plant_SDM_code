3a6d4876726f360edaab9ec55f04bfce31e0ba7e
Hugh Burley <hugh.burley@gmail.com>
2017-07-05 17:13:50 +1000

    Added INSTALL_PACKAGES.R
    - Use this file to install and load multiple packages at once

---
+++ R/INSTALL_PACKAGES.R
@@ -0,0 +1,42 @@
+#########################################################################################################################
+###################################### INSTALL MULTIPLE PACKAGES AT ONCE  ############################################### 
+#########################################################################################################################
+
+
+## create a mega list of all the packages you could ever need?
+packages <- c("ggplot2",   "plyr",   "reshape2", "RColorBrewer",  "scales",     "grid",
+              "raster",    "spdep",  "rgdal",    "GISTools",      "data.table", "dtplyr",
+              "Hmisc",     "Cairo",  "lattice",  "gtools",        "ggplot2",    "sjPlot",
+              "gridExtra", "grid",   "sqldf",    "knitr",         "mgcv",       "MuMIn",
+              "mvabund",   "asbio",  "mvtnorm",  "testthat",      "rgl",        "R.matlab",
+              "ENMeval",   "lavaan", "semPlot",  "rgdal",         "sp",         "formula.tools",
+              "dismo",     "mctest", "rJava",    "ENMeval",
+              "statisticalModeling") # class(packages)
+
+
+## now use a function from the internet that takes a list of packages, installs and loads them
+ipak <- function(pkg){
+  
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  
+  sapply(pkg, require, character.only = TRUE)
+  
+}
+
+
+## run the function on the list.....
+ipak(packages)
+
+
+## check that R maxent is installed
+list.files(system.file("java", package = "dismo"))
+ENMevaluate
+
+
+## now commit these changes...
+
+
+
