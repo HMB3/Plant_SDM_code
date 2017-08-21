5572fdbc788e6a8af6a3bb3913c610dd908f9044
Hugh Burley <hugh.burley@gmail.com>
2017-08-22 08:48:33 +1000

    Update GBIF_FILTER.R
    - Need to add comments for all of John's steps, track that it is achieving what Stuart was doing

--- R/GBIF_FILTER.R
+++ R/GBIF_FILTER.R
@@ -4,7 +4,7 @@
 
 
 #########################################################################################################################
-## 1). CHECK SEARCHED AND RETURNED NAMES FROM GBIF, AND DUPLICATES
+## 1). CHECK SEARCHED AND RETURNED NAMES FROM GBIF
 #########################################################################################################################
 
 
@@ -74,48 +74,49 @@ summary(GBIF.ALL$lon)[7]/dim(GBIF.ALL)[1]*100   ## 11% of data has no lat/long
 summary(GBIF.ALL$year)[7]/dim(GBIF.ALL)[1]*100  ## 23% of data has no year
 
 
+# #########################################################################################################################
+# ## Now create columns on which to exclude records
+# GBIF.ALL %<>%
+#   
+#   ## all the columns names on the right hand side must match GBIF
+#   mutate(
+#     
+#     #CLEAN_REMOVE_NOT_AVH = !(dataProvider == "WORLDralia's Virtual Herbarium" & !is.na(dataProvider)),
+#     
+#     #CLEAN_REMOVE_NOT_IN_TAXON_LIST = !scientificName %in% taxa$taxonName,
+#     
+#     CLEAN_REMOVE_BASISOFRECORD = basisOfRecord == "HUMAN_OBSERVATION",  ## TRUE for HUMAN OBSERVATION
+#     
+#     CLEAN_REMOVE_MISSING_LONLAT = is.na(lon) | is.na(lat),              ## TRUE = na(lon/lat)
+#     
+#     #CLEAN_REMOVE_COORD = coordinateUncertaintyInMeters <= 1000,
+#     
+#     CLEAN_REMOVE_CULTIVATED_FLAG = establishmentMeans == "MANAGED",
+#     
+#     #CLEAN_REMOVE_CULTIVATED_LOCALITY = NA,
+#     
+#     CLEAN_REMOVE_PRE_1950 = year < 1950 & !is.na(year),                 ## TRUE = na(year)
+#     
+#     FLAG_MISSING_YEAR = is.na(year)
+#     
+#     #ASSERT_TAXON_ID_ISSUE = !taxonIdentificationIssue %in% c("noIssue", "matchedToMisappliedName"),
+#     
+#     # ASSERT_COORD_ISSUE = ifelse(countryCoordinateMismatch | stateCoordinateMismatch | coordinatesCentreOfStateProvince == "TRUE" |
+#     #                             coordinatesCentreOfCountry == "TRUE" | negatedLongitude == "TRUE" | negatedLatitude == "TRUE" |
+#     #                             unrecognizedGeodeticDatum, TRUE, FALSE),
+#     
+#     #ASSERT_COORD_ISSUE = ifelse(is.na(ASSERT_COORD_ISSUE), FALSE, ASSERT_COORD_ISSUE),
+#     
+#     #ASSERT_DATE_FIRST_CENTURY = ifelse(!is.na(firstOfCentury) & firstOfCentury & year == 1900, TRUE, FALSE)
+#     
+#   )
+
+
+
+
+
 #########################################################################################################################
-## Now create columns on which to exclude records
-GBIF.ALL %<>%
-  
-  ## all the columns names on the right hand side must match GBIF
-  mutate(
-    
-    #CLEAN_REMOVE_NOT_AVH = !(dataProvider == "WORLDralia's Virtual Herbarium" & !is.na(dataProvider)),
-    
-    #CLEAN_REMOVE_NOT_IN_TAXON_LIST = !scientificName %in% taxa$taxonName,
-    
-    CLEAN_REMOVE_BASISOFRECORD = basisOfRecord == "HUMAN_OBSERVATION",  ## TRUE for HUMAN OBSERVATION
-    
-    CLEAN_REMOVE_MISSING_LONLAT = is.na(lon) | is.na(lat),              ## TRUE = na(lon/lat)
-    
-    #CLEAN_REMOVE_COORD = coordinateUncertaintyInMeters <= 1000,
-    
-    CLEAN_REMOVE_CULTIVATED_FLAG = establishmentMeans == "MANAGED",
-    
-    #CLEAN_REMOVE_CULTIVATED_LOCALITY = NA,
-    
-    CLEAN_REMOVE_PRE_1950 = year < 1950 & !is.na(year),                 ## TRUE = na(year)
-    
-    FLAG_MISSING_YEAR = is.na(year)
-    
-    #ASSERT_TAXON_ID_ISSUE = !taxonIdentificationIssue %in% c("noIssue", "matchedToMisappliedName"),
-    
-    # ASSERT_COORD_ISSUE = ifelse(countryCoordinateMismatch | stateCoordinateMismatch | coordinatesCentreOfStateProvince == "TRUE" |
-    #                             coordinatesCentreOfCountry == "TRUE" | negatedLongitude == "TRUE" | negatedLatitude == "TRUE" |
-    #                             unrecognizedGeodeticDatum, TRUE, FALSE),
-    
-    #ASSERT_COORD_ISSUE = ifelse(is.na(ASSERT_COORD_ISSUE), FALSE, ASSERT_COORD_ISSUE),
-    
-    #ASSERT_DATE_FIRST_CENTURY = ifelse(!is.na(firstOfCentury) & firstOfCentury & year == 1900, TRUE, FALSE)
-    
-  )
-
-
-
-
-#########################################################################################################################
-## 4). CLEAN RECORDS
+## 2). CLEAN RECORDS
 #########################################################################################################################
 
 
@@ -129,34 +130,44 @@ GBIF.CLEAN %<>%
   ## can't apply the filters that only apply to ALA data
   filter(!CLEAN_REMOVE_MISSING_LONLAT)
 
+##
 problems <- with(GBIF.TRIM,
+                 
                  table(
+                   
                    is.na(lon)|is.na(lat),
-                   establishmentMeans=='MANAGED' & !is.na(establishmentMeans),
+                   establishmentMeans == 'MANAGED' & !is.na(establishmentMeans),
                    year < 1950 & !is.na(year),
                    is.na(year),
                    coordinateUncertaintyInMeters > 1000 & 
                      !is.na(coordinateUncertaintyInMeters)
-                   )
-                 ) %>% 
-  as.data.frame %>% 
+                   
+                 )
+                 
+) %>% 
+  
+  as.data.frame %>%  
+  
   setNames(c('lonlat', 'establishment', 
              'pre1950', 'noyear', 'coord_uncertainty', 'count'))
 
-write.csv(problems, 'problems.csv', row.names=FALSE)
 
+## write file to CSV
+write.csv(problems, 'problems.csv', row.names = FALSE)
+
+
+## clean 
 GBIF.CLEAN <- GBIF.HIA.SPP.RECORDS.ALL %>% 
+  
   select(one_of(
+    
     c('basisOfRecord', 'lon', 'lat', 'establishmentMeans', 
       'year', 'scientificName', 'searchTaxon', 'gbifID', 
       'coordinateUncertaintyInMeters'))) %>% 
+  
   filter(!is.na(lon) & !is.na(lat),
          establishmentMeans!='MANAGED' | is.na(establishmentMeans),
          year >= 1950 & !is.na(year))
-
-
-
-
 
 
 
@@ -175,10 +186,23 @@ xy <- cellFromXY(world.temp, GBIF.CLEAN[c("lon", "lat")]) %>%
   xyFromCell(world.temp, .)
 
 #onland <- cells[!is.na(world.temp[cells])]  ## placeholder for what's been passed from previous argument
-vals <- gdalUtils::gdallocationinfo(
-  "//sci-7910/F/data/worldclim/world/0.5/bio/current/bio_01",
-  coords=xy,
-  valonly=TRUE, raw_output=FALSE, geoloc=T, wgs84=T)
+# vals <- gdalUtils::gdallocationinfo(
+#   "//sci-7910/F/data/worldclim/world/0.5/bio/current/bio_01",
+#   coords=xy,
+#   valonly=TRUE, raw_output=FALSE, geoloc=T, wgs84=T)
+# 
+
+
+##
+onland <- extract(world.temp, xy) %>% !is.na %>% xy[.,]
+onland = z %>% is.na %>%  `!` # %>% xy[.,]  cells on land or not
+
+onland.points = filter(GBIF.CLEAN, cellFromXY(world.temp, GBIF.CLEAN[c("lon", "lat")]) %in% 
+                         unique(cellFromXY(world.temp, GBIF.CLEAN[c("lon", "lat")]))[onland]) 
+  
+
+##
+points(onland.points[c("lon", "lat")], pch = ".")
 
 
 ## clip to coastline
