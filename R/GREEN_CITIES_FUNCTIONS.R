32433df31c758295c070a672985a780669f1751e
Hugh Burley <hugh.burley@gmail.com>
2017-10-20 12:08:42 +1100

    Update GREEN_CITIES_FUNCTIONS.R
    - Added some basic plotting functions

--- R/GREEN_CITIES_FUNCTIONS.R
+++ R/GREEN_CITIES_FUNCTIONS.R
@@ -57,6 +57,90 @@ string_fun_first_word <- function(x) {
 
 
 
+#########################################################################################################################
+## CORRELATION PLOTTING FUNCTIONS
+#########################################################################################################################
+
+
+## if(missing(cex.cor)) cex = 0.8/strwidth(txt)
+## text(0.5, 0.5, txt, cex = 5) 
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
+  
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = 2)
+}
+
+
+panel.loess = function(x, y, digits=2, prefix="", cex.cor) {
+  
+  usr = par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  
+  r = (loess(x, y))
+  
+  txt = format(c(r, 0.123456789), digits=digits)[1]
+  txt = paste(prefix, txt, sep = "")
+  
+  if(missing(cex.cor)) cex = 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = 5)
+  
+}
+
+
+panel.sig <- function(x, y, digits = 2, cex.cor)
+  
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  test <- cor.test(x,y)
+  Signif <- ifelse(round(test$p.value,3)<0.001,"p<0.001",paste("p=",round(test$p.value,3)))  
+  text(0.5, 0.25, paste("r=",txt))
+  text(.5, .75, Signif)
+  
+}
+
+
+## tweak these arguments: lwd = 8, cex, etc.
+panel.smooth <- function (x, y, col = "blue", bg = NA, pch = 19, 
+                          cex = 1.2, col.smooth = "red",
+                          cex.axis = 3,
+                          span = 2/3, iter = 3, ...) 
+  
+{
+  points(x, y, pch = pch, col = col, bg = bg, cex = cex, cex.axis = cex.axis)
+  
+  ok <- is.finite(x) & is.finite(y)
+  if (any(ok)) 
+    lines(stats::lowess(x[ok], y[ok], f = span, iter = iter), 
+          col = "orange", lwd = 4, cex.axis = 3, ...)
+  
+}
+
+
+## tweak these arguments: border = NA, colour = grey, etc.
+## cannot figure out how to get the axes of these plots to enlarge...
+panel.hist <- function(x, ...)
+  
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, #col = "grey", 
+       border = "NA", ...)
+  
+}
+
+
 
 
 #########################################################################################################################
