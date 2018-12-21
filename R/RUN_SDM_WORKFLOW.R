4d89dcb2c466403ff8169e7b0194c5beaccb2a2d
z3221316 <hugh.burley@gmail.com>
2018-12-21 14:24:13 +1100

    Update MAPPING_FUNCTIONS.R
    - Add code to create mess maps for every species
    - If possible, we can use the MESS to mask out every species. Then, use the mess for mapping and SUA layers

--- R/RUN_SDM_WORKFLOW.R
+++ R/RUN_SDM_WORKFLOW.R
@@ -275,6 +275,8 @@ source('./R/8_MAP_SDM_COMBINE.R', echo = TRUE)
 
 ## Find points that make the code not reproducible - look for all hard coded paths
 
+## Can we use the mess maps to create the maps and sua steps?
+
 ## Flatten the structure for an example species
 
 ## Improve the raster extract step
